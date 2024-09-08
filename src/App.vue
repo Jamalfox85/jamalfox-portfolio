5ceb067e0e80578eb3863369ef71968a1a11780d
Jamal <Fox.Jamal@outlook.com>
2024-09-07 21:19:51 -0400

    image test 5

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main_wrapper">
-    <img class="bg" :src="'./images/default-mac-bg.jpeg'" alt="background" />
+    <img class="bg" :src="'/images/default-mac-bg.jpeg'" alt="background" />
     <div class="desktop_wrapper">
       <desktop-view />
     </div>
