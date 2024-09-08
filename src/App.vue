75167f3b4a395f839fd36abfe0e27803904211eb
Jamal <Fox.Jamal@outlook.com>
2024-09-07 21:41:18 -0400

    image test 7

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="main_wrapper">
-    <img class="bg" src="./../public/images/default-mac-bg.jpeg" alt="background" />
+    <img class="bg" src="./../public/images/default-mac-bg.jpg" alt="background" />
     <div class="desktop_wrapper">
       <desktop-view />
     </div>
     <div class="client_wrapper custom-cursor" v-if="clientOpen">
-      <img class="bg" src="./../public/images/gameflow-background.jpeg" alt="background" />
+      <img class="bg" src="./../public/images/gameflow-background.jpg" alt="background" />
       <top-bar />
       <div class="client-main">
         <RouterView />
